9349f51f974621221f3a8e4907b169a405f1db9f
nicoandresr <nicoandres.rodriguez@gmail.com>
2017-01-08 12:04:41 -0500

    Create menu button

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,11 +2,13 @@ import { NgModule, ErrorHandler } from '@angular/core';
 import { IonicApp, IonicModule, IonicErrorHandler } from 'ionic-angular';
 import { MyApp } from './app.component';
 import { HomePage } from '../pages/home/home';
+import { FabComponent } from '../components/fab/';
 
 @NgModule({
   declarations: [
     MyApp,
-    HomePage
+    HomePage,
+    FabComponent
   ],
   imports: [
     IonicModule.forRoot(MyApp)
---
+++ src/components/fab/fab.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'fab',
+  templateUrl: 'fab.html'
+})
+export class FabComponent {
+
+  public constructor() {
+  }
+
+}
